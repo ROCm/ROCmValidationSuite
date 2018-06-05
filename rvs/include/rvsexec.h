ab5fe2b9e42f3d353aa08f995e08530d102f185a
Milica <m-visekruna@hdl-dh.com>
2018-06-05 11:54:41 -0400

    support for -t command line option

--- rvs/include/rvsexec.h
+++ rvs/include/rvsexec.h
@@ -32,13 +32,12 @@ protected:
 	
 	void 	do_help(void);
 	void 	do_version(void);
-	void 	do_list_modules(void);
+
 	int 	do_yaml(const string& config_file);
 	int 	do_yaml_properties(const YAML::Node& node, const string& module_name, if1* pif1);
 	bool	is_yaml_properties_collection(const string& module_name, const string& proprty_name);
 	int 	do_yaml_properties_collection(const YAML::Node& node, const string& parent_name, if1* pif1);
 
-	
 };
 
 	
