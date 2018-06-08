347bc26e3717821de258985aab7582eccca5dfd9
Mario HDL <m-lucin@hdl-dh.com>
2018-06-08 11:23:07 -0400

    Added support for 'wait' action property

--- src/rvslib.cpp
+++ src/rvslib.cpp
@@ -2,6 +2,9 @@
 
 #include <rvslib.h>
 
+#include <chrono>
+#include <unistd.h>
+
 using namespace std;
 
 rvs::lib::actionbase::actionbase()
@@ -16,4 +19,8 @@ int rvs::lib::actionbase::property_set(const char* pKey, const char* pVal)
 {
 	property.insert( pair<string, string>(pKey, pVal));
 	return 0;
+}
+
+void rvs::lib::actionbase::sleep(const unsigned int ms) {
+	::usleep(1000*ms);
 }
