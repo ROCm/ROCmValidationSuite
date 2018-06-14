1eb598ea65735f2e797ee1610dda69b11a6bb358
Tudor Ioan Cerlinca <t-cerlinca@hdl-dh.com>
2018-06-14 02:55:01 -0400

    PEQT (added regex support, validations and code refactoring)

--- peqt.so/include/action.h
+++ peqt.so/include/action.h
@@ -2,7 +2,21 @@
 #ifndef PEQT_SO_INCLUDE_ACTION_H_
 #define PEQT_SO_INCLUDE_ACTION_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include <pci/pci.h>
+#ifdef __cplusplus
+}
+#endif
+
+#include <vector>
+#include <string>
+
 #include "rvslib.h"
+
+using std::vector;
+using std::string;
 
 class action: public rvs::lib::actionbase {
  public:
@@ -11,6 +25,21 @@ class action: public rvs::lib::actionbase {
 
     virtual int property_set(const char*, const char*);
     virtual int run(void);
+
+ private:
+    vector<string> device_prop_gpu_id_list;  // the list of all gpu_id
+                                             // in the <device> property
+
+    string action_name;
+
+    // PCIe capabilities stuff
+    bool get_gpu_all_pcie_capabilities(struct pci_dev *dev);
+
+    // configuration properties getters
+    bool property_get_device(int *error); // gets the device property value (list of gpu_id)
+    //from the module's properties collection
+    void property_get_action_name(void);  // gets the action name
+    int property_get_deviceid(int *error);  // gets the deviceid
 
  protected:
 };
--- src/rvs_util.cpp
+++ src/rvs_util.cpp
@@ -1,6 +1,11 @@
+// Copyright [year] <Copyright Owner> ... goes here
+#include <vector>
+#include <string>
+
 #include "rvs_util.h"
 
-using namespace std;
+using std::vector;
+using std::string;
 
 /**
  * splits a string based on a given delimiter
@@ -8,9 +13,9 @@ using namespace std;
  * @param delimiter tokens' delimiter
  * @return vector containing all tokens
  */
-std::vector<string> str_split(const string& str_val, const string& delimiter) {
-    std::vector<string> str_tokens;
-    unsigned short int prev_pos = 0, cur_pos = 0;
+vector<string> str_split(const string& str_val, const string& delimiter) {
+    vector<string> str_tokens;
+    unsigned int prev_pos = 0, cur_pos = 0;
     do {
         cur_pos = str_val.find(delimiter, prev_pos);
         if (cur_pos == string::npos)
