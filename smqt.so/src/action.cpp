b188fda887130f5b4f8720184e5838897f289df3
Milica <m-visekruna@hdl-dh.com>
2018-06-18 04:11:04 -0400

    small corrections on smqt

--- smqt.so/src/action.cpp
+++ smqt.so/src/action.cpp
@@ -67,6 +67,33 @@ int action::run(void)
   struct pci_dev *dev;  
   dev = pacc->devices; 
   
+  //get requsted values 
+  for (auto it=property.begin(); it!=property.end(); ++it){
+    if(it->first == "name")
+      action_name = it->second;
+    if (it->first=="bar1_req_size") 
+      bar1_req_size=std::atoi (it->second.c_str());        
+    if (it->first=="bar2_req_size") 
+      bar2_req_size=std::atoi (it->second.c_str());
+    if (it->first=="bar4_req_size") 
+      bar4_req_size=std::atoi (it->second.c_str());
+    if (it->first=="bar5_req_size") 
+      bar5_req_size=std::atoi (it->second.c_str());
+    
+    if (it->first=="bar1_base_addr_min") 
+      bar1_base_addr_min=std::atoi (it->second.c_str());
+    if (it->first=="bar2_base_addr_min") 
+      bar2_base_addr_min=std::atoi (it->second.c_str());
+    if (it->first=="bar4_base_addr_min") 
+      bar4_base_addr_min=std::atoi (it->second.c_str());
+    
+    if (it->first=="bar1_base_addr_max") 
+      bar1_base_addr_max=std::atoi (it->second.c_str());
+    if (it->first=="bar2_base_addr_max") 
+      bar2_base_addr_max=std::atoi (it->second.c_str());
+    if (it->first=="bar4_base_addr_max") 
+      bar4_base_addr_max=std::atoi (it->second.c_str());        
+  }
   
   //iterate over devices
   for (dev = pacc->devices; dev; dev = dev->next) {
@@ -89,86 +116,54 @@ int action::run(void)
     bar4_base_addr = dev->base_addr[5];
     bar4_size = dev->size[5]; 
     bar5_size = dev->rom_size;
- 
-    for(int i=0;i<7;i++){
-      cout<<"addr["<<i<<"]"<<dev->base_addr[i]<<endl;    
-      cout<<"size"<<i<<"]"<<dev->size[i]<<endl;       
-    }
+    
+    //check if values are as expected  
+    pass=true;
+    
+    if ((bar1_base_addr<bar1_base_addr_min)||(bar1_base_addr>bar1_base_addr_max))
+      pass=false;
+    if ((bar2_base_addr<bar1_base_addr_min)||(bar2_base_addr>bar2_base_addr_max))
+      pass=false;
+    if ((bar4_base_addr<bar4_base_addr_min)||(bar4_base_addr>bar4_base_addr_max))
+      pass=false;
+    
+    if ((bar1_req_size>bar1_size)||(bar2_req_size<bar2_size)||(bar4_req_size<bar4_size)||(bar5_req_size<bar5_size))
+      pass=false;
+    
+    
+    //loginfo
+    unsigned int sec;
+    unsigned int usec;
+    rvs::lp::get_ticks(sec, usec);
+    string msgs1,msgs2,msgs4,msgs5,msga1,msga2,msga4,pmsg;
+    msgs1 = "[" + action_name + "] " + " smqt bar1_size " + std::to_string(bar1_size);
+    msgs2 = "[" + action_name + "] " + " smqt bar2_size " + std::to_string(bar2_size);
+    msgs4 = "[" + action_name + "] " + " smqt bar4_size " + std::to_string(bar4_size);
+    msgs5 = "[" + action_name + "] " + " smqt bar5_size " + std::to_string(bar5_size);
+    msga1 = "[" + action_name + "] " + " smqt bar1_base_addr " + std::to_string(bar1_base_addr);
+    msga2 = "[" + action_name + "] " + " smqt bar2_base_addr " + std::to_string(bar2_base_addr);
+    msga4 = "[" + action_name + "] " + " smqt bar4_base_addr " + std::to_string(bar4_base_addr);
+    pmsg = "[" + action_name + "] " + " smqt " + std::to_string(pass);
+    void* r = rvs::lp::LogRecordCreate("SMQT", action_name.c_str(), rvs::loginfo, sec, usec);
+    rvs::lp::Log( msgs1.c_str(), rvs::loginfo, sec, usec);
+    rvs::lp::Log( msga1.c_str(), rvs::loginfo, sec, usec);
+    rvs::lp::Log( msgs2.c_str(), rvs::loginfo, sec, usec);
+    rvs::lp::Log( msga2.c_str(), rvs::loginfo, sec, usec);
+    rvs::lp::Log( msgs4.c_str(), rvs::loginfo, sec, usec);
+    rvs::lp::Log( msga4.c_str(), rvs::loginfo, sec, usec);
+    rvs::lp::Log( msgs5.c_str(), rvs::loginfo, sec, usec);
+    rvs::lp::Log( pmsg.c_str(), rvs::logresults);    
+    rvs::lp::AddString(r, "bar1_size", std::to_string(bar1_size));
+    rvs::lp::AddString(r, "bar1_base_addr", std::to_string(bar1_base_addr));
+    rvs::lp::AddString(r, "bar2_size", std::to_string(bar2_size));
+    rvs::lp::AddString(r, "bar2_base_addr", std::to_string(bar2_base_addr));
+    rvs::lp::AddString(r, "bar4_size", std::to_string(bar4_size));
+    rvs::lp::AddString(r, "bar4_base_addr", std::to_string(bar4_base_addr));
+    rvs::lp::AddString(r, "bar5_size", std::to_string(bar4_size));
+    rvs::lp::AddString(r, "pass", std::to_string(pass));
+    rvs::lp::LogRecordFlush(r);
     
   } 
-
-
-  //get requsted values 
-  for (auto it=property.begin(); it!=property.end(); ++it){
-    if(it->first == "name")
-      action_name = it->second;
-    if (it->first=="bar1_req_size") 
-      bar1_req_size=std::atoi (it->second.c_str());        
-    if (it->first=="bar2_req_size") 
-      bar2_req_size=std::atoi (it->second.c_str());
-    if (it->first=="bar4_req_size") 
-      bar4_req_size=std::atoi (it->second.c_str());
-    if (it->first=="bar5_req_size") 
-      bar5_req_size=std::atoi (it->second.c_str());
-        
-    if (it->first=="bar1_base_addr_min") 
-      bar1_base_addr_min=std::atoi (it->second.c_str());
-    if (it->first=="bar2_base_addr_min") 
-      bar2_base_addr_min=std::atoi (it->second.c_str());
-    if (it->first=="bar4_base_addr_min") 
-      bar4_base_addr_min=std::atoi (it->second.c_str());
-
-    if (it->first=="bar1_base_addr_max") 
-      bar1_base_addr_max=std::atoi (it->second.c_str());
-    if (it->first=="bar2_base_addr_max") 
-      bar2_base_addr_max=std::atoi (it->second.c_str());
-    if (it->first=="bar4_base_addr_max") 
-      bar4_base_addr_max=std::atoi (it->second.c_str());        
-  }
-
-  
-  //checking if values are as expected  
-  pass=true;
-  
-  if ((bar1_base_addr<bar1_base_addr_min)||(bar1_base_addr>bar1_base_addr_max))
-    pass=false;
-  if ((bar2_base_addr<bar1_base_addr_min)||(bar2_base_addr>bar2_base_addr_max))
-    pass=false;
-  if ((bar4_base_addr<bar4_base_addr_min)||(bar4_base_addr>bar4_base_addr_max))
-    pass=false;
-  
-  if ((bar1_req_size>bar1_size)||(bar2_req_size<bar2_size)||(bar4_req_size<bar4_size)||(bar5_req_size<bar5_size))
-    pass=false;
-  
-  
-  //loginfo  
-  string msgs1,msgs2,msgs4,msgs5,msga1,msga2,msga4,pmsg;
-  msgs1 = "[" + action_name + "] " + " smqt bar1_size " + std::to_string(bar1_size);
-  msgs2 = "[" + action_name + "] " + " smqt bar2_size " + std::to_string(bar2_size);
-  msgs4 = "[" + action_name + "] " + " smqt bar4_size " + std::to_string(bar4_size);
-  msgs5 = "[" + action_name + "] " + " smqt bar5_size " + std::to_string(bar5_size);
-  msga1 = "[" + action_name + "] " + " smqt bar1_base_addr " + std::to_string(bar1_base_addr);
-  msga2 = "[" + action_name + "] " + " smqt bar2_base_addr " + std::to_string(bar2_base_addr);
-  msga4 = "[" + action_name + "] " + " smqt bar4_base_addr " + std::to_string(bar4_base_addr);
-  pmsg = "[" + action_name + "] " + " smqt " + std::to_string(pass);
-  void* r = rvs::lp::LogRecordCreate("SMQT", action_name.c_str(), rvs::loginfo,0,0);
-  rvs::lp::Log( msgs1.c_str(), rvs::loginfo);
-  rvs::lp::Log( msga1.c_str(), rvs::loginfo);
-  rvs::lp::Log( msgs2.c_str(), rvs::loginfo);
-  rvs::lp::Log( msga2.c_str(), rvs::loginfo);
-  rvs::lp::Log( msgs4.c_str(), rvs::loginfo);
-  rvs::lp::Log( msga4.c_str(), rvs::loginfo);
-  rvs::lp::Log( msgs5.c_str(), rvs::loginfo);
-  rvs::lp::Log( pmsg.c_str(), rvs::logresults);    
-  rvs::lp::AddString(r, "bar1_size", std::to_string(bar1_size));
-  rvs::lp::AddString(r, "bar1_base_addr", std::to_string(bar1_base_addr));
-  rvs::lp::AddString(r, "bar2_size", std::to_string(bar2_size));
-  rvs::lp::AddString(r, "bar2_base_addr", std::to_string(bar2_base_addr));
-  rvs::lp::AddString(r, "bar4_size", std::to_string(bar4_size));
-  rvs::lp::AddString(r, "bar4_base_addr", std::to_string(bar4_base_addr));
-  rvs::lp::AddString(r, "bar5_size", std::to_string(bar4_size));
-  rvs::lp::AddString(r, "pass", std::to_string(pass));
-  rvs::lp::LogRecordFlush(r);
     
   return pass;
     
