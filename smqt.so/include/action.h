cd3bdf12b5c37547f45d6dda145cf7981df6b260
Milica <m-visekruna@hdl-dh.com>
2018-07-13 08:25:42 -0400

    refactoring and Doxygen

--- smqt.so/include/action.h
+++ smqt.so/include/action.h
@@ -26,17 +26,23 @@
 #define ACTION_H_
 
 #include "rvsactionbase.h"
+/**
+ * @class action
+ * @ingroup SMQT
+ *
+ * @brief SMQT action implementation class
+ *
+ * Derives from rvs::actionbase and implements actual action functionality
+ * in its run() method.
+ *
+ */
+class action : public rvs::actionbase {
+  public:
+    action();
+    virtual ~action();
+    virtual int run(void);
 
-class action : public rvs::actionbase
-{
-public:
-  action();
-  virtual ~action();
-  
-  virtual int run(void);
-  
-protected:
-
+  protected:
 };
 
 #endif /* ACTION_H_ */
