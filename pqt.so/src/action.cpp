3cd1506b5e7d7209f3966bc4a2f1b89128801b50
Dusan Matic <d-matic@hdl-dh.com>
2018-07-26 17:58:40 +0200

    bidir added

--- pqt.so/src/action.cpp
+++ pqt.so/src/action.cpp
@@ -94,7 +94,7 @@ int action::run(void) {
           // send p2p traffic (unidirectional)
           send_p2p_traffic(src_agent, dst_agent, src_buff, dst_buff, false, src_max_size, dst_max_size, false);
           // send p2p traffic (bidirectional)
-//           send_p2p_traffic(src_agent, dst_agent, src_buff, dst_buff, true, src_max_size, dst_max_size, false);          
+          send_p2p_traffic(src_agent, dst_agent, src_buff, dst_buff, true, src_max_size, dst_max_size, false);          
         }
       }
     }
@@ -473,10 +473,12 @@ double action::GetCopyTime(bool bidirectional, hsa_signal_t signal_fwd, hsa_sign
  * */
 void action::send_p2p_traffic(hsa_agent_t src_agent, hsa_agent_t dst_agent, hsa_amd_memory_pool_t src_buff, hsa_amd_memory_pool_t dst_buff, bool bidirectional, size_t src_max_size, size_t dst_max_size, bool validate) {
   hsa_status_t status;
-  void* src_pool_pointer;
-  void* dst_pool_pointer;
+  void* src_pool_pointer_fwd;
+  void* dst_pool_pointer_fwd;
+  void* src_pool_pointer_rev;
+  void* dst_pool_pointer_rev;  
   string log_msg;
-  hsa_signal_t signal_fwd, signal_rev;;
+  hsa_signal_t signal_fwd, signal_rev;
   char s_buff[256];
   uint64_t total_size = 0;
   double total_time = 0;  
@@ -499,7 +501,6 @@ void action::send_p2p_traffic(hsa_agent_t src_agent, hsa_agent_t dst_agent, hsa_
 
   // Initialize size of buffer to equal the largest element of allocation
   uint32_t size_len = size_list.size();
-//   uint32_t max_size = size_list.back();
 
   // Iterate through the differnt buffer sizes to
   // compute the bandwidth as determined by copy
@@ -518,20 +519,38 @@ void action::send_p2p_traffic(hsa_agent_t src_agent, hsa_agent_t dst_agent, hsa_
     log("[PQT] -----------------------------------------------------------------------------------", rvs::logdebug);  
 
     // Allocate buffers in src and dst pools
-    status = hsa_amd_memory_pool_allocate(src_buff, curr_size, 0, (void**)&src_pool_pointer);
+    status = hsa_amd_memory_pool_allocate(src_buff, curr_size, 0, (void**)&src_pool_pointer_fwd);
     print_hsa_status("[PQT] send_p2p_traffic - hsa_amd_memory_pool_allocate(SRC)", status);
-    snprintf(s_buff, sizeof(s_buff), "%p", src_pool_pointer);
-    rvs::lp::Log(std::string("src_pool_pointer = ") + s_buff, rvs::logdebug);
+    snprintf(s_buff, sizeof(s_buff), "%p", src_pool_pointer_fwd);
+    rvs::lp::Log(std::string("src_pool_pointer_fwd = ") + s_buff, rvs::logdebug);
 
-    status = hsa_amd_memory_pool_allocate(dst_buff, curr_size, 0, (void**)&dst_pool_pointer);
+    status = hsa_amd_memory_pool_allocate(dst_buff, curr_size, 0, (void**)&dst_pool_pointer_fwd);
     print_hsa_status("[PQT] send_p2p_traffic - hsa_amd_memory_pool_allocate(DST)", status);
-    snprintf(s_buff, sizeof(s_buff), "%p", dst_pool_pointer);
-    rvs::lp::Log(std::string("dst_pool_pointer = ") + s_buff, rvs::logdebug);
+    snprintf(s_buff, sizeof(s_buff), "%p", dst_pool_pointer_fwd);
+    rvs::lp::Log(std::string("dst_pool_pointer_fwd = ") + s_buff, rvs::logdebug);
 
+    if (bidirectional == true) {
+      status = hsa_amd_memory_pool_allocate(src_buff, curr_size, 0, (void**)&src_pool_pointer_rev);
+      print_hsa_status("[PQT] send_p2p_traffic BIDIRECTIONAL - hsa_amd_memory_pool_allocate(SRC)", status);
+      snprintf(s_buff, sizeof(s_buff), "%p", src_pool_pointer_rev);
+      rvs::lp::Log(std::string("src_pool_pointer_rev = ") + s_buff, rvs::logdebug);
+
+      status = hsa_amd_memory_pool_allocate(dst_buff, curr_size, 0, (void**)&dst_pool_pointer_rev);
+      print_hsa_status("[PQT] send_p2p_traffic BIDIRECTIONAL - hsa_amd_memory_pool_allocate(DST)", status);
+      snprintf(s_buff, sizeof(s_buff), "%p", dst_pool_pointer_rev);
+      rvs::lp::Log(std::string("dst_pool_pointer_rev = ") + s_buff, rvs::logdebug);
+    }
+      
+    
     // Initialize buffers if validate is used
     if (validate == true) {
-      std::memset(src_pool_pointer, 0xA5, curr_size);
-      std::memset(dst_pool_pointer, 0x5A, curr_size);
+      std::memset(src_pool_pointer_fwd, 0xA5, curr_size);
+      std::memset(dst_pool_pointer_fwd, 0x5A, curr_size);
+      
+      if (bidirectional == true) {
+        std::memset(src_pool_pointer_rev, 0xA5, curr_size);
+        std::memset(dst_pool_pointer_rev, 0x5A, curr_size);
+      }
     }
 
     // Create a signal to wait on copy operation
@@ -540,13 +559,27 @@ void action::send_p2p_traffic(hsa_agent_t src_agent, hsa_agent_t dst_agent, hsa_
     print_hsa_status("[PQT] send_p2p_traffic - hsa_signal_create()", status);
     
     // get agent access
-    status = hsa_amd_agents_allow_access(1, &src_agent, NULL, dst_pool_pointer);
+    status = hsa_amd_agents_allow_access(1, &src_agent, NULL, dst_pool_pointer_fwd);
     print_hsa_status("[PQT] send_p2p_traffic - hsa_amd_agents_allow_access(SRC)", status);
 
-    status = hsa_amd_agents_allow_access(1, &dst_agent, NULL, src_pool_pointer);
+    status = hsa_amd_agents_allow_access(1, &dst_agent, NULL, src_pool_pointer_fwd);
     print_hsa_status("[PQT] send_p2p_traffic - hsa_amd_agents_allow_access(DST)", status);
+
     // store signal
     hsa_signal_store_relaxed(signal_fwd, 1);
+    
+    if (bidirectional == true) {
+      status = hsa_signal_create(1, 0, NULL, &signal_rev);
+      print_hsa_status("[PQT] send_p2p_traffic BIDIRECTIONAL - hsa_signal_create()", status);
+    
+      status = hsa_amd_agents_allow_access(1, &src_agent, NULL, dst_pool_pointer_rev);
+      print_hsa_status("[PQT] send_p2p_traffic BIDIRECTIONAL - hsa_amd_agents_allow_access(SRC)", status);
+
+      status = hsa_amd_agents_allow_access(1, &dst_agent, NULL, src_pool_pointer_rev);
+      print_hsa_status("[PQT] send_p2p_traffic BIDIRECTIONAL - hsa_amd_agents_allow_access(DST)", status);
+      
+      hsa_signal_store_relaxed(signal_rev, 1);
+    }
 
     // TODO need to check if the combination is unidirectonial or bidirectional
     
@@ -560,6 +593,17 @@ void action::send_p2p_traffic(hsa_agent_t src_agent, hsa_agent_t dst_agent, hsa_
       log(log_msg.c_str(), rvs::logdebug);
       return;
     }
+
+    if (bidirectional == true) {
+      status = hsa_amd_agent_memory_pool_get_info(dst_agent, src_buff, HSA_AMD_AGENT_MEMORY_POOL_INFO_ACCESS, &access);
+      print_hsa_status("[PQT] send_p2p_traffic BIDIRECTIONAL - hsa_amd_agent_memory_pool_get_info(DST->SRC)", status);
+
+      if (access == HSA_AMD_MEMORY_POOL_ACCESS_NEVER_ALLOWED) {
+        log_msg = "[PQT] send_p2p_traffic BIDIRECTIONAL - HSA_AMD_MEMORY_POOL_ACCESS_NEVER_ALLOWED for DST -> SRC), so skip it ...";
+        log(log_msg.c_str(), rvs::logdebug);
+        return;
+      }
+    }
     
     // Start the timer and launch forward copy operation
     total_size += curr_size;
@@ -567,15 +611,28 @@ void action::send_p2p_traffic(hsa_agent_t src_agent, hsa_agent_t dst_agent, hsa_
     
     // Copy from src into dst buffer
     // hsa_amd_memory_async_copy(void* dst, hsa_agent_t dst_agent, const void* src, hsa_agent_t src_agent, size_t size, uint32_t num_dep_signals, const hsa_signal_t* dep_signals, hsa_signal_t completion_signal)
-    status = hsa_amd_memory_async_copy(dst_pool_pointer, dst_agent, src_pool_pointer, src_agent, curr_size, 0, NULL, signal_fwd);
+    status = hsa_amd_memory_async_copy(dst_pool_pointer_fwd, dst_agent, src_pool_pointer_fwd, src_agent, curr_size, 0, NULL, signal_fwd);
     print_hsa_status("[PQT] send_p2p_traffic - hsa_amd_memory_async_copy(SRC -> DST)", status);
 
+    if (bidirectional == true) {
+      status = hsa_amd_memory_async_copy(src_pool_pointer_rev, src_agent, dst_pool_pointer_rev, dst_agent, curr_size, 0, NULL, signal_rev);
+      print_hsa_status("[PQT] send_p2p_traffic BIDIRECTIONAL - hsa_amd_memory_async_copy(DST -> SRC)", status);      
+    }
+    
     // Wait for the forward copy operation to complete
     log_msg = "[PQT] send_p2p_traffic - hsa_signal_wait_acquire(SRC -> DST) before ...";
     log(log_msg.c_str(), rvs::logdebug);
     while (hsa_signal_wait_acquire(signal_fwd, HSA_SIGNAL_CONDITION_LT, 1, uint64_t(-1), HSA_WAIT_STATE_ACTIVE));
     log_msg = "[PQT] send_p2p_traffic - hsa_signal_wait_acquire(SRC -> DST) after ...";
     log(log_msg.c_str(), rvs::logdebug);
+    
+    if (bidirectional == true) {
+      log_msg = "[PQT] send_p2p_traffic BIDIRECTIONAL - hsa_signal_wait_acquire(DST -> SRC) before ...";
+      log(log_msg.c_str(), rvs::logdebug);
+      while (hsa_signal_wait_acquire(signal_rev, HSA_SIGNAL_CONDITION_LT, 1, uint64_t(-1), HSA_WAIT_STATE_ACTIVE));
+      log_msg = "[PQT] send_p2p_traffic BIDIRECTIONAL - hsa_signal_wait_acquire(DST -> SRC) after ...";
+      log(log_msg.c_str(), rvs::logdebug);
+    }    
     
     // Stop the timer object
     timer.StopTimer(index);
@@ -597,8 +654,12 @@ void action::send_p2p_traffic(hsa_agent_t src_agent, hsa_agent_t dst_agent, hsa_
     log(log_msg.c_str(), rvs::logdebug);    
     
     // Compare output equals input
-    if (validate == true && memcmp(src_pool_pointer, dst_pool_pointer, curr_size) != 0) {
-      log("After copy is finished memory content is not the same", rvs::logerror);
+    if (validate == true && memcmp(src_pool_pointer_fwd, dst_pool_pointer_fwd, curr_size) != 0) {
+      log("After copy is finished memory content is not the same (forward)", rvs::logerror);
+    }
+    // Compare output equals input
+    if (bidirectional == true && validate == true && memcmp(src_pool_pointer_rev, dst_pool_pointer_rev, curr_size) != 0) {
+      log("After copy is finished memory content is not the same (forward)", rvs::logerror);
     }
     
   }
