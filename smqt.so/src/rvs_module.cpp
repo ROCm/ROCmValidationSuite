080e69a183a3deca7f719bea92b6104cc9b10d8b
fvranichdl <f-vranic@hdl-dh.com>
2018-07-30 16:57:32 +0200

    Fixing cpplint - breaking lines

--- smqt.so/src/rvs_module.cpp
+++ smqt.so/src/rvs_module.cpp
@@ -56,11 +56,17 @@ extern "C" const char* rvs_module_get_description(void) {
 }
 
 extern "C" const char* rvs_module_get_config(void) {
-  return (const char*)"bar1_req_size(integer), bar1_base_addr_min(integer), bar1_base_addr_max(integer), bar2_req_size(integer), bar2_base_addr_min(integer), bar2_base_addr_max(integer), bar4_req_size(integer), bar4_base_addr_min(integer), bar4_base_addr_max(integer), bar5_req_size(integer)";
+  return (const char*)"bar1_req_size(integer), bar1_base_addr_min(integer), "
+  "bar1_base_addr_max(integer), bar2_req_size(integer), bar2_base_addr_min("
+  "integer), bar2_base_addr_max(integer), bar4_req_size(integer), "
+  "bar4_base_addr_min(integer), bar4_base_addr_max(integer), "
+  "bar5_req_size(integer)";
 }
 
 extern "C" const char* rvs_module_get_output(void) {
-  return (const char*)"bar1_size(integer), bar1_base_addr(integer), bar2_size(integer), bar2_base_addr(integer), bar4_size(integer), bar4_base_addr(integer), bar5_size(integer), pass(bool)";
+  return (const char*)"bar1_size(integer), bar1_base_addr(integer), bar2_size"
+  "(integer), bar2_base_addr(integer), bar4_size(integer), bar4_base_addr"
+  "(integer), bar5_size(integer), pass(bool)";
 }
 
 extern "C" int   rvs_module_init(void* pMi) {
