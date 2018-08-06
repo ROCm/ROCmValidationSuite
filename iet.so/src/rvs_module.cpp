d8a4199b6223a67263b0d93e1ccf2ad14366eb85
Tudor Ioan Cerlinca <t-cerlinca@hdl-dh.com>
2018-08-06 15:21:07 +0200

    Added IET skeleton

---
+++ iet.so/src/rvs_module.cpp
@@ -0,0 +1,113 @@
+/********************************************************************************
+ *
+ * Copyright (c) 2018 ROCm Developer Tools
+ *
+ * MIT LICENSE:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *******************************************************************************/
+#include "rvs_module.h"
+#include "action.h"
+#include "rvsloglp.h"
+#include "gpu_util.h"
+
+/**
+ * @defgroup IET IET Module
+ *
+ * @brief performs Input EDPp Test
+ *
+ * The Input EDPp Test can be used to characterize the peak power
+ * capabilities of a GPU to different levels of use. The purpose
+ * of the IET module is to bring the GPU(s) to a preconfigured power
+ * level in watts by gradually increasing the compute load on the GPUs
+ * until the desired power level is achieved. This verifies that the GPUs
+ * can sustain a power level for a reasonable amount of time without
+ * problems like thermal violations arising.
+ *
+ */
+
+int log(const char* pMsg, const int level) {
+    return rvs::lp::Log(pMsg, level);
+}
+
+extern "C" void  rvs_module_get_version(int* Major, int* Minor, int* Revision) {
+    *Major    = BUILD_VERSION_MAJOR;
+    *Minor    = BUILD_VERSION_MINOR;
+    *Revision = BUILD_VERSION_PATCH;
+}
+
+extern "C" int rvs_module_has_interface(int iid) {
+    switch (iid) {
+    case 0:
+    case 1:
+        return 1;
+    }
+
+    return 0;
+}
+
+extern "C" const char* rvs_module_get_name(void) {
+    return "iet";
+}
+
+extern "C" const char* rvs_module_get_description(void) {
+    return "ROCm Validation Suite IET module";
+}
+
+extern "C" const char* rvs_module_get_config(void) {
+    return "target_power (float), ramp_interval (int), "\
+            "tolerance (float), max_violations (int), "\
+            "sample_interval (int), log_interval (int)";
+}
+
+extern "C" const char* rvs_module_get_output(void) {
+    return "pass (bool)";
+}
+
+extern "C" int rvs_module_init(void* pMi) {
+    rvs::lp::Initialize(static_cast<T_MODULE_INIT*>(pMi));
+    rvs::gpulist::Initialize();
+    return 0;
+}
+
+extern "C" int rvs_module_terminate(void) {
+    return 0;
+}
+
+extern "C" const char* rvs_module_get_errstring(int error) {
+    return "General Error";
+}
+
+extern "C" void* rvs_module_action_create(void) {
+    return static_cast<void*>(new action);
+}
+
+extern "C" int   rvs_module_action_destroy(void* pAction) {
+    delete static_cast<rvs::actionbase*>(pAction);
+    return 0;
+}
+
+extern "C" int rvs_module_action_property_set(void* pAction, const char* Key,
+                                                            const char* Val) {
+    return static_cast<rvs::actionbase*>(pAction)->property_set(Key, Val);
+}
+
+extern "C" int rvs_module_action_run(void* pAction) {
+    return static_cast<rvs::actionbase*>(pAction)->run();
+}
