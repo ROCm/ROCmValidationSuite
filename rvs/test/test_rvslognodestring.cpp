6aa19482bb9caa7702205ba6feccb01863237ce3
igorhdl <rvs_dev@hdl-dh.com>
2018-12-06 11:45:23 +0100

    test update

--- rvs/test/test_rvslognodestring.cpp
+++ rvs/test/test_rvslognodestring.cpp
@@ -28,10 +28,10 @@
 #include "gtest/gtest.h"
 
 #include "rvslognodebase.h"
-#include "rvslognodeint.h"
+#include "rvslognodestring.h"
 #include "rvs_unit_testing_defs.h"
 
-class ext_lognode : public rvs::LogNodeInt {
+class ext_lognode : public rvs::LogNodeString {
  public:
   std::string get_Name() {
     return Name;
@@ -45,30 +45,30 @@ class ext_lognode : public rvs::LogNodeInt {
     return Type;
   }
 
-  int get_Value() {
+  std::string get_String() {
     return Value;
   }
 };
 
-class LogNodeIntTest : public ::testing::Test {
+class LogNodeStringTest : public ::testing::Test {
  protected:
   void SetUp() override {
-    level_1 = new rvs::LogNodeInt("level_1", 10, nullptr);
+    level_1 = new rvs::LogNodeString("level_1", "10", nullptr);
 
-    level_2[0] = new rvs::LogNodeInt("level_2_0", 20, level_1);
-    level_2[1] = new rvs::LogNodeInt("level_2_1", 21, level_1);
-    level_2[2] = new rvs::LogNodeInt("level_2_2", 22, level_1);
+    level_2[0] = new rvs::LogNodeString("level_2_0", "20", level_1);
+    level_2[1] = new rvs::LogNodeString("level_2_1", "21", level_1);
+    level_2[2] = new rvs::LogNodeString("level_2_2", "22", level_1);
 
-    level_3[0] = new rvs::LogNodeInt("level_3_0", 30, level_2[0]);
-    level_3[1] = new rvs::LogNodeInt("level_3_1", 31, level_2[0]);
-    level_3[2] = new rvs::LogNodeInt("level_3_2", 32, level_2[1]);
-    level_3[3] = new rvs::LogNodeInt("level_3_3", 33, level_2[2]);
-    level_3[4] = new rvs::LogNodeInt("level_3_4", 34, level_2[2]);
+    level_3[0] = new rvs::LogNodeString("level_3_0", "30", level_2[0]);
+    level_3[1] = new rvs::LogNodeString("level_3_1", "31", level_2[0]);
+    level_3[2] = new rvs::LogNodeString("level_3_2", "32", level_2[1]);
+    level_3[3] = new rvs::LogNodeString("level_3_3", "33", level_2[2]);
+    level_3[4] = new rvs::LogNodeString("level_3_4", "34", level_2[2]);
 
-    level_4[0] = new rvs::LogNodeInt("level_4_0", 40, level_3[2]);
-    level_4[1] = new rvs::LogNodeInt("level_4_1", 41, level_3[4]);
+    level_4[0] = new rvs::LogNodeString("level_4_0", "40", level_3[2]);
+    level_4[1] = new rvs::LogNodeString("level_4_1", "41", level_3[4]);
 
-    empty = new rvs::LogNodeInt("empty", -1, nullptr);
+    empty = new rvs::LogNodeString("empty", "-1", nullptr);
   }
 
   void TearDown() override {
@@ -86,15 +86,15 @@ class LogNodeIntTest : public ::testing::Test {
   }
 
   // tree structure of nodes
-  rvs::LogNodeInt* level_1;
-  rvs::LogNodeInt* level_2[3];
-  rvs::LogNodeInt* level_3[5];
-  rvs::LogNodeInt* level_4[2];
+  rvs::LogNodeString* level_1;
+  rvs::LogNodeString* level_2[3];
+  rvs::LogNodeString* level_3[5];
+  rvs::LogNodeString* level_4[2];
   // empty node
-  rvs::LogNodeInt* empty;
+  rvs::LogNodeString* empty;
 };
 
-TEST_F(LogNodeIntTest, log_node_base) {
+TEST_F(LogNodeStringTest, log_node_base) {
   std::string json_string;
   std::string exp_string;
   ext_lognode* node;
@@ -105,39 +105,44 @@ TEST_F(LogNodeIntTest, log_node_base) {
   // ----------------------------------------
   node = static_cast<ext_lognode*>(empty);
   EXPECT_STREQ(node->get_Name().c_str(), "empty");
-  EXPECT_EQ(node->get_Value(), static_cast<int>(-1));
+  exp_string = "-1";
+  EXPECT_STREQ(node->get_String().c_str(), exp_string.c_str());
   tmp_node = static_cast<ext_lognode*>(node->get_Parent());
   EXPECT_EQ(tmp_node, nullptr);
   json_string = node->ToJson("Test ");
-  EXPECT_STREQ(json_string.c_str(), "\nTest \"empty\" : -1");
+  EXPECT_STREQ(json_string.c_str(), "\nTest \"empty\" : \"-1\"");
 
   // ----------------------------------------
   // check name and type for each node
   // ----------------------------------------
   node = static_cast<ext_lognode*>(level_1);
   EXPECT_STREQ(node->get_Name().c_str(), "level_1");
-  EXPECT_EQ(node->get_Type(), rvs::eLN::Integer);
-  EXPECT_EQ(node->get_Value(), static_cast<int>(10));
+  EXPECT_EQ(node->get_Type(), rvs::eLN::String);
+  exp_string = "10";
+  EXPECT_STREQ(node->get_String().c_str(), exp_string.c_str());
   for (uint i = 0; i < 3; i++) {
     exp_string = "level_2_" + std::to_string(i);
     node = static_cast<ext_lognode*>(level_2[i]);
     EXPECT_STREQ(node->get_Name().c_str(), exp_string.c_str());
-    EXPECT_EQ(node->get_Type(), rvs::eLN::Integer);
-    EXPECT_EQ(node->get_Value(), static_cast<int>(20+i));
+    EXPECT_EQ(node->get_Type(), rvs::eLN::String);
+    exp_string = "2" + std::to_string(i);
+    EXPECT_STREQ(node->get_String().c_str(), exp_string.c_str());
   }
   for (uint i = 0; i < 5; i++) {
     exp_string = "level_3_" + std::to_string(i);
     node = static_cast<ext_lognode*>(level_3[i]);
     EXPECT_STREQ(node->get_Name().c_str(), exp_string.c_str());
-    EXPECT_EQ(node->get_Type(), rvs::eLN::Integer);
-    EXPECT_EQ(node->get_Value(), static_cast<int>(30+i));
+    EXPECT_EQ(node->get_Type(), rvs::eLN::String);
+    exp_string = "3" + std::to_string(i);
+    EXPECT_STREQ(node->get_String().c_str(), exp_string.c_str());
   }
   for (uint i = 0; i < 2; i++) {
     exp_string = "level_4_" + std::to_string(i);
     node = static_cast<ext_lognode*>(level_4[i]);
     EXPECT_STREQ(node->get_Name().c_str(), exp_string.c_str());
-    EXPECT_EQ(node->get_Type(), rvs::eLN::Integer);
-    EXPECT_EQ(node->get_Value(), static_cast<int>(40+i));
+    EXPECT_EQ(node->get_Type(), rvs::eLN::String);
+    exp_string = "4" + std::to_string(i);
+    EXPECT_STREQ(node->get_String().c_str(), exp_string.c_str());
   }
 
   // ----------------------------------------
@@ -153,7 +158,7 @@ TEST_F(LogNodeIntTest, log_node_base) {
     tmp_node = static_cast<ext_lognode*>(node->get_Parent());
     exp_string = "level_1";
     EXPECT_STREQ(tmp_node->get_Name().c_str(), exp_string.c_str());
-    EXPECT_EQ(tmp_node->get_Type(), rvs::eLN::Integer);
+    EXPECT_EQ(tmp_node->get_Type(), rvs::eLN::String);
   }
   // 3_0, 3_1 parents
   for (uint i = 0; i < 2; i++) {
@@ -161,7 +166,7 @@ TEST_F(LogNodeIntTest, log_node_base) {
     tmp_node = static_cast<ext_lognode*>(node->get_Parent());
     exp_string = "level_2_0";
     EXPECT_STREQ(tmp_node->get_Name().c_str(), exp_string.c_str());
-    EXPECT_EQ(tmp_node->get_Type(), rvs::eLN::Integer);
+    EXPECT_EQ(tmp_node->get_Type(), rvs::eLN::String);
   }
   // 3_2 parents
   for (uint i = 2; i < 3; i++) {
@@ -169,7 +174,7 @@ TEST_F(LogNodeIntTest, log_node_base) {
     tmp_node = static_cast<ext_lognode*>(node->get_Parent());
     exp_string = "level_2_1";
     EXPECT_STREQ(tmp_node->get_Name().c_str(), exp_string.c_str());
-    EXPECT_EQ(tmp_node->get_Type(), rvs::eLN::Integer);
+    EXPECT_EQ(tmp_node->get_Type(), rvs::eLN::String);
   }
   // 3_3, 3_4 parents
   for (uint i = 3; i < 5; i++) {
@@ -177,36 +182,36 @@ TEST_F(LogNodeIntTest, log_node_base) {
     tmp_node = static_cast<ext_lognode*>(node->get_Parent());
     exp_string = "level_2_2";
     EXPECT_STREQ(tmp_node->get_Name().c_str(), exp_string.c_str());
-    EXPECT_EQ(tmp_node->get_Type(), rvs::eLN::Integer);
+    EXPECT_EQ(tmp_node->get_Type(), rvs::eLN::String);
   }
 
   // ----------------------------------------
   // check json
   // ----------------------------------------
   json_string = level_1->ToJson();
-  EXPECT_STREQ(json_string.c_str(), "\n\"level_1\" : 10");
+  EXPECT_STREQ(json_string.c_str(), "\n\"level_1\" : \"10\"");
 
   json_string = level_2[0]->ToJson();
-  EXPECT_STREQ(json_string.c_str(), "\n\"level_2_0\" : 20");
+  EXPECT_STREQ(json_string.c_str(), "\n\"level_2_0\" : \"20\"");
   json_string = level_2[1]->ToJson();
-  EXPECT_STREQ(json_string.c_str(), "\n\"level_2_1\" : 21");
+  EXPECT_STREQ(json_string.c_str(), "\n\"level_2_1\" : \"21\"");
   json_string = level_2[2]->ToJson();
-  EXPECT_STREQ(json_string.c_str(), "\n\"level_2_2\" : 22");
+  EXPECT_STREQ(json_string.c_str(), "\n\"level_2_2\" : \"22\"");
 
   json_string = level_3[0]->ToJson();
-  EXPECT_STREQ(json_string.c_str(), "\n\"level_3_0\" : 30");
+  EXPECT_STREQ(json_string.c_str(), "\n\"level_3_0\" : \"30\"");
   json_string = level_3[1]->ToJson();
-  EXPECT_STREQ(json_string.c_str(), "\n\"level_3_1\" : 31");
+  EXPECT_STREQ(json_string.c_str(), "\n\"level_3_1\" : \"31\"");
   json_string = level_3[2]->ToJson();
-  EXPECT_STREQ(json_string.c_str(), "\n\"level_3_2\" : 32");
+  EXPECT_STREQ(json_string.c_str(), "\n\"level_3_2\" : \"32\"");
   json_string = level_3[3]->ToJson();
-  EXPECT_STREQ(json_string.c_str(), "\n\"level_3_3\" : 33");
+  EXPECT_STREQ(json_string.c_str(), "\n\"level_3_3\" : \"33\"");
   json_string = level_3[4]->ToJson();
-  EXPECT_STREQ(json_string.c_str(), "\n\"level_3_4\" : 34");
+  EXPECT_STREQ(json_string.c_str(), "\n\"level_3_4\" : \"34\"");
 
   json_string = level_4[0]->ToJson();
-  EXPECT_STREQ(json_string.c_str(), "\n\"level_4_0\" : 40");
+  EXPECT_STREQ(json_string.c_str(), "\n\"level_4_0\" : \"40\"");
   json_string = level_4[1]->ToJson();
-  EXPECT_STREQ(json_string.c_str(), "\n\"level_4_1\" : 41");
+  EXPECT_STREQ(json_string.c_str(), "\n\"level_4_1\" : \"41\"");
 }
 
