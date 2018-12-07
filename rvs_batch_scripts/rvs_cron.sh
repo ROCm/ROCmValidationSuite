a2e8f3a79e3e1d26ddf3d17fae895df60200ef82
Mario HDL <m-lucin@hdl-dh.com>
2018-12-07 16:03:21 +0100

    bugfix

--- rvs_batch_scripts/rvs_cron.sh
+++ rvs_batch_scripts/rvs_cron.sh
@@ -13,9 +13,9 @@ mkdir -p ${RVS_BATCH_BUILD}
 export RVS_BATCH_UTC=`date -u`
 echo " 2. run ubuntu "${RVS_BATCH_UTC} >> $RVS_BATCH_SCRIPTS/cron.log
 cd ${RVS_BATCH_SCRIPTS}
-#${RVS_BATCH_SCRIPTS}/rvs_ubuntu.sh
+${RVS_BATCH_SCRIPTS}/rvs_ubuntu.sh
 
-# runc CentOS Docker image and test script
+# run CentOS Docker image and test script
 export RVS_BATCH_UTC=`date -u`
 echo " 3. run centos "${RVS_BATCH_UTC} >> $RVS_BATCH_SCRIPTS/cron.log
 docker run --privileged=true -it --network=host --device=/dev/kfd --device=/dev/dri --group-add video -v /work/rvs_batch_scripts:/work/batch_scripts -v /home/user1:/home/root d4fb34eba628 /work/batch_scripts/rvs_centos.sh  >> $RVS_BATCH_SCRIPTS/cron.log
