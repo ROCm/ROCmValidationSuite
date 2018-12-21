2fd86188c8c7e8fa9ef5ec929430c16ae9d120e3
igorhdl <rvs_dev@hdl-dh.com>
2018-12-21 11:06:51 +0100

    update

--- regression/run_and_check_test.py
+++ regression/run_and_check_test.py
@@ -16,24 +16,35 @@ test_json_file_name = "tmp_json_result.txt"
 # passed arguments:
 # --------------------
 #   rvs bin path
+#   rvs path
 #   conf
 #   log_usage
 #   json usage
 # --------------------
 
 bin_path    = sys.argv[1]
-conf_name   = sys.argv[2]
-log_usage   = sys.argv[3]
-json_usage  = sys.argv[4]
+rvs_path    = sys.argv[2]
+conf_name   = sys.argv[3]
+log_usage   = sys.argv[4]
+json_usage  = sys.argv[5]
 
 # ./run_single_test /work/igorhdl/ROCm2/build/bin /work/igorhdl/ROCm2/ROCmValidationSuite/rvs/conf/rand_pqt0.conf json_tmp.txt tmp.txt
+
+# get current location
+curr_location = subprocess.check_output(["pwd", ""])
+curr_location_size = len(curr_location)
+curr_location = curr_location[0:curr_location_size-1]
+print curr_location
 
 # run test (with json)
 if json_usage == 'true':
    test_cmd = "./run_single_test %s %s %s %s" % (bin_path, conf_name,  bin_path + "/" + test_json_file_name, bin_path + "/" + test_result_file_name)
 else:
    test_cmd = "./run_single_test %s %s %s %s" % (bin_path, conf_name,  "no_json", bin_path + "/" + test_result_file_name)
+
+os.chdir(rvs_path + "/regression")
 os.system(test_cmd)
+os.chdir(curr_location)
 
 # result test pass/fail
 test_result = True
