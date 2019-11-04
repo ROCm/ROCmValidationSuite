a3216a79139b57b5c012fd50e988715813fe486f
Rakesh Seethamsetty <vseetham@amd.com>
2019-11-04 11:43:53 -0800

    Instead of using wget to get this file, these are stored in the same repo

---
+++ testscripts/iet.new.sh
@@ -0,0 +1,6 @@
+echo 'iet_1';sudo ./rvs -c conf/iet_1.conf -d 3 && date
+echo 'iet_2';sudo ./rvs -c conf/iet_2.conf -d 3 && date
+echo 'iet_3';sudo ./rvs -c conf/iet_3.conf -d 3 && date
+echo 'iet_4';sudo ./rvs -c conf/iet_4.conf -d 3 && date
+echo 'iet_5';sudo ./rvs -c conf/iet_5.conf -d 3 && date
+echo 'iet_6';sudo ./rvs -c conf/iet_6.conf -d 3 && date
---
+++ testscripts/pesm.new.sh
@@ -0,0 +1,11 @@
+date
+echo 'pesm_1';sudo ./rvs -c conf/pesm_1.conf -d 3; date
+echo 'pesm_2';sudo ./rvs -c conf/pesm_2.conf -d 3; date
+echo 'pesm_3';sudo ./rvs -c conf/pesm_3.conf -d 3; date
+echo 'pesm_4';sudo ./rvs -c conf/pesm_4.conf -d 3; date
+echo 'pesm_5';sudo ./rvs -c conf/pesm_5.conf -d 3; date
+echo 'pesm_6';sudo ./rvs -c conf/pesm_5.conf -d 3; date
+echo 'pesm_7';sudo ./rvs -c conf/pesm_7.conf -d 3; date
+echo 'pesm_8';sudo ./rvs -c conf/pesm_8.conf -d 3; date
+echo 'pesm_9';sudo ./rvs -c conf/pesm_9.conf -d 3; date
+
