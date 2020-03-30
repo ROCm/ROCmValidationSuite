f70ccba54966eacfa8cc78e168cbaea7f34f65e5
Paresh Patel <pareshgpatelec@gmail.com>
2020-03-30 12:18:28 +0000

    fixed deviceid.sh for whitespace isse and added peer_deviceid. deviceid also

--- rvs/conf/deviceid.sh
+++ rvs/conf/deviceid.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
-# To use this utlity call it from testcript with below example from build/bin
-# Example:  ./conf/deviceid.sh "conf/gpup_*.conf"
-# Above command will add device_id : XXXX in all gpup_*.conf files
+# to Use this script need to set RVS_EXE_PATH where yor rvs executable is.
+# ex. RVS_EXE_PATH=/opt/rocm/rvs/rvs for default rvs installation path
+# Run script as below
+# Example:  deviceid.sh "conf/gpup_*.conf"<-- conf file path
+# Above command will replace device_id/device_id/deviceid : XXXX in all gpup_*.conf files
 # To change any single file use as below example
 # ./conf/deviceid.sh conf/gpup_1.conf
  
+function get_string_with_space {
+local str=$1 ; shift
+local line=$1; shift
+local new_str=$1
+echo $new_str
+echo "After new string"
+printf "%s" "$line"
+local space=" "
+ret_str=""
+str_space=0
+str_space=(`printf "%s" "$line" | awk -F ":" '{print $1}' | grep "$str" | awk -F'[^ ]' '{print length($1),NR}'`)
+echo $str_space
+str_space=$(($str_space + 0))
+while [ $str_space -gt 0 ]
+do
+    new_str=$(printf "%s%s" "$space" "$new_str")
+    str_space=$(($str_space - 1))
+    echo $str_space
+done
+echo "before new_str"
+printf "%s" "$new_str"
+ret_str=$new_str
+}
+
 #Below  configLine function I got from https://stackoverflow.com/a/54909102
 # configLine will check for device_id string in current file and replace or
 # add device_id if not present
@@ -12,12 +38,23 @@ function configLine {
     local OLD_LINE_PATTERN=$1; shift
     local NEW_LINE=$1; shift
     local FILE=$1
-    local NEW=$(echo "${NEW_LINE}" | sed 's/\//\\\//g')
-    sed -i '/'"${OLD_LINE_PATTERN}"'/{s/.*/'"${NEW}"'/;h};${x;/./{x;q100};x}' "${FILE}"
-    if [[ $? -ne 100 ]] && [[ ${NEW_LINE} != '' ]]
-    then
-        echo "${NEW_LINE}" >> "${FILE}"
-    fi
+    local line_num=1
+    while IFS= read -r line
+    do
+        printf "%s" $line > file.txt
+        
+        if printf "%s" "$line" | awk -F ":" '{print $1}' | grep "\b$OLD_LINE_PATTERN\b" ; then
+            if  [[ $line !=  *"#"* ]] ; then 
+                #echo ${OLD_LINE_PATTERN} 
+                #echo "$line"
+                echo $NEW_LINE
+                echo "before get_string_with_space"
+                get_string_with_space "$OLD_LINE_PATTERN" "$line" "$NEW_LINE"
+                sed -i "${line_num}s/.*/${ret_str}/" "${FILE}"
+            fi
+        fi
+        let "line_num=line_num+1"
+    done < "$FILE"
 }
 
 gpu_dev_id_list=()
@@ -48,13 +85,23 @@ done
 # Below code is to get available device and update device: with correct value 
 # in .conf files passed as argument to shell script
 dev_num_list=()
-dev_num_list=(`./rvs -g | grep Device |  awk '{ print $6 }' | grep -o -E '[0-9]+'`)
+if [[ -z "${RVS_EXE_PATH}" ]]; then
+  echo "ERROR: RVS_EXE_PATH not set, First set RVS_EXE_PATH environment path"
+  exit 0
+else
+  rvs_exe_path="${RVS_EXE_PATH}"
+fi
+dev_num_list=(`$rvs_exe_path -g | grep Device |  awk '{ print $6 }' | grep -o -E '[0-9]+'`)
 dev_num_str=${dev_num_list[@]}
 #echo "dev_num_str $dev_num_str"
 
 # Below for loop is for going through all .conf files passed as argument to
 # deviceid.sh file
 for dev_id_line in $1; do
-    configLine  "device_id" "  device_id: $d_i" $dev_id_line
-    configLine  "device:" "  device: $dev_num_str" $dev_id_line
+    configLine  "device_id" "device_id: $d_i" $dev_id_line
+    configLine  "deviceid" "deviceid: $d_i" $dev_id_line
+    configLine  "peer_deviceid" "peer_deviceid: $d_i" $dev_id_line
+    configLine  "peer_device_id" "peer_device_id: $d_i" $dev_id_line
+    configLine  "device" "device: $dev_num_str" $dev_id_line
+    configLine  "peers" "peers: $dev_num_str" $dev_id_line
 done
