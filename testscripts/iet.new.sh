79d5ae1efa8c92b96a48f46c749f39d473abf474
Paresh Patel <38318725+pareshgp@users.noreply.github.com>
2020-05-01 00:18:08 +0530

    Device ID , test scripts, packagae name change

--- testscripts/iet.new.sh
+++ testscripts/iet.new.sh
@@ -1,7 +1,3 @@
-./conf/deviceid.sh "conf/iet_*.conf"
-echo 'iet_1';sudo ./rvs -c conf/iet_1.conf -d 3 && date
-echo 'iet_2';sudo ./rvs -c conf/iet_2.conf -d 3 && date
-echo 'iet_3';sudo ./rvs -c conf/iet_3.conf -d 3 && date
-echo 'iet_4';sudo ./rvs -c conf/iet_4.conf -d 3 && date
-echo 'iet_5';sudo ./rvs -c conf/iet_5.conf -d 3 && date
-echo 'iet_6';sudo ./rvs -c conf/iet_6.conf -d 3 && date
+date
+./conf/deviceid.sh conf/iet_single.conf
+echo 'iet';sudo ./rvs -c conf/iet_single.conf -d 3 && date
