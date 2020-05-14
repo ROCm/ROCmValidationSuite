1cb7816b7434228b0a352dd81f543c628882e75d
Rakesh Seethasmetty <vseetham@amd.com>
2020-05-14 15:59:58 -0700

    Revert "Fix IET hang issue"
    This reverts commit 43dfc8924ef2cf06be9162be5c4306973a1b2ff6.

--- iet.so/src/blas_worker.cpp
+++ iet.so/src/blas_worker.cpp
@@ -211,15 +211,14 @@ void blas_worker::run() {
 
     for (;;) {
 
-	      msg = " Running blas worker thread ";
+	msg = " Running blas worker thread ";
         rvs::lp::Log(msg, rvs::loginfo);
 
         {
             std::lock_guard<std::mutex> lck(mtx_brun);
             if (!brun) {
-	              msg = " Blas worker stop signal recieved " ;
+	        msg = " Blas worker stop signal recieved " ;
                 rvs::lp::Log(msg, rvs::loginfo);
-                gpu_blas.release();
                 break;
 	    }
         }
