ec782b693585c99582a2463d30b7a92cbe576120
Vasudevan Visvanathan <vasudevan.visvanathan@gmail.com>
2020-06-12 13:01:52 +0530

    Argument for Long duration stress test
    Users can specify the duration as 1 or 2 which maps to hours for execution

--- testscripts/rvs-stress-long.sh
+++ testscripts/rvs-stress-long.sh
@@ -1,2 +1,22 @@
-date
-echo 'gst-3hrs';sudo ./rvs -c conf/gst_stress_3_hrs.conf -d 3; date
+if [ "$1" == "-h" ] ; then
+        echo
+        echo "Usage: ./`basename $0` 3 (for 3 hours) or ./`basename $0` 6 (for 6 hours), default duration is 1 hour"
+        echo
+    exit 0
+fi
+if [ "$#" -eq 0 ] ; then
+    duration="3600000"
+    msval="msval"
+    sed -i "s/$msval/${duration}/g" conf/gst_stress.conf
+    date
+    echo 'gst-1hr';sudo ./rvs -c conf/gst_stress.conf -d 3; date
+    sed -i "s/${duration}/$msval/g" conf/gst_stress.conf
+else
+    duration=$((3600000 * $1))
+    echo $duration
+    msval="msval"
+    sed -i "s/$msval/${duration}/g" conf/gst_stress.conf
+    date
+    echo 'gst-'$1hr'(s)';sudo ./rvs -c conf/gst_stress.conf -d 3; date
+    sed -i "s/${duration}/$msval/g" conf/gst_stress.conf
+fi
