211a633da7e19b906a7b0da4766bc9d4dcea7716
Paresh Patel <38318725+pareshgp@users.noreply.github.com>
2020-07-14 22:23:14 +0530

    Adding gpup_single.conf and fixing pebb.new.sh and rvs-mem.new.sh (#464)

--- testscripts/gpup.new.sh
+++ testscripts/gpup.new.sh
@@ -1,9 +1,3 @@
 date
-./conf/deviceid.sh "conf/gpup_*.conf"
-echo 'gpup_1';sudo ./rvs -c conf/gpup_1.conf -d 3; date
-echo 'gpup_2';sudo ./rvs -c conf/gpup_2.conf -d 3; date
-echo 'gpup_3';sudo ./rvs -c conf/gpup_3.conf -d 3; date
-echo 'gpup_4';sudo ./rvs -c conf/gpup_4.conf -d 3; date
-echo 'gpup_5';sudo ./rvs -c conf/gpup_5.conf -d 3; date
-echo 'gpup_6';sudo ./rvs -c conf/gpup_6.conf -d 3; date
-echo 'gpup_7';sudo ./rvs -c conf/gpup_7.conf -d 3; date
+./conf/deviceid.sh conf/gpup_single.conf
+echo 'gpup';sudo ./rvs -c conf/gpup_single.conf -d 3; date 
