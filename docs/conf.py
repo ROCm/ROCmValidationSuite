765effea20b3d43eae008bc755b17405fd3212e7
Sam Wu <sam.wu2@amd.com>
2024-01-15 09:04:16 -0700

    Standardize documentation for ReadtheDocs (#677)
    Relates to https://github.com/RadeonOpenCompute/rocm-docs-core/issues/330

--- docs/conf.py
+++ docs/conf.py
@@ -4,10 +4,31 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import re
+import subprocess
+
 from rocm_docs import ROCmDocs
 
-docs_core = ROCmDocs("RVS Documentation")
+with open('../CMakeLists.txt', encoding='utf-8') as f:
+    match = re.search(r'.*\bset \( RVS_VERSION\s+\"?([0-9.]+)[^0-9.]+', f.read())
+    if not match:
+        raise ValueError("VERSION not found!")
+    version_number = match[1]
+left_nav_title = f"RVS {version_number} Documentation"
+
+# for PDF output on Read the Docs
+project = "RVS Documentation"
+author = "Advanced Micro Devices, Inc."
+copyright = "Copyright (c) 2023 Advanced Micro Devices, Inc. All rights reserved."
+version = version_number
+release = version_number
+
+external_toc_path = "./sphinx/_toc.yml"
+
+docs_core = ROCmDocs(left_nav_title)
 docs_core.setup()
+
+external_projects_current_project = "rocmvalidationsuite"
 
 for sphinx_var in ROCmDocs.SPHINX_VARS:
     globals()[sphinx_var] = getattr(docs_core, sphinx_var)
