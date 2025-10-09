22ef552f356aa450ece2ad2fae46076e2ca38ed4
Mohammed Junaid <Junaid.Kottikulam@amd.com>
2025-10-09 01:48:00 -0500

    Test levels for MI3XX

--- rvs/src/rvsexec.cpp
+++ rvs/src/rvsexec.cpp
@@ -1,6 +1,6 @@
 /********************************************************************************
  *
- * Copyright (c) 2018-2024 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (c) 2018-2025 Advanced Micro Devices, Inc. All rights reserved.
  *
  * MIT LICENSE:
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
@@ -127,34 +127,76 @@ int rvs::exec::run() {
   if (rvs::options::has_option("-j", &s_json_log_file)) {
     logger::to_json(true);
     logger::set_json_log_file(s_json_log_file);
-    
   }
 
-  // check -c option
   string config_file;
-  if (rvs::options::has_option("-c", &val)) {
-    config_file = val;
-  } else {
-    config_file = "../share/rocm-validation-suite/conf/rvs.conf";
+  // check -r option
+  if (rvs::options::has_option("-r", &val)) {
+
+    std::string plaftorm_name = get_gpu_name();
+
+    if(val != "1" && val != "2" && val != "3" && val != "4" && val != "5") {
+      rvs::logger::Err("Test level must be between 1 and 5 !", MODULE_NAME_CAPS);
+      return -1;
+    }
+
+    if(plaftorm_name.empty()) {
+      rvs::logger::Err("No test levels support for the detected GPU or no GPU detected !", MODULE_NAME_CAPS);
+      return -1;
+    }
+
+    config_file = "../share/rocm-validation-suite/conf/" + plaftorm_name + "/levels/rvs_level_" + val + ".conf";
+
     // Check if pConfig file exist if not use old path for backward compatibility
     std::ifstream file(path + config_file);
     if (!file.good()) {
-      config_file = "conf/rvs.conf";
+      config_file = "conf/" + plaftorm_name + "/levels/rvs_level_" + val + ".conf";
     }
     file.close();
     config_file = path + config_file;
+
+    // Check if pConfig file exists
+    file.open(config_file);
+    if (!file.good()) {
+      char buff[1024];
+      snprintf(buff, sizeof(buff),
+          "%s file is missing.", config_file.c_str());
+      rvs::logger::Err(buff, MODULE_NAME_CAPS);
+      return -1;
+    } else {
+      file.close();
+    }
   }
 
-  // Check if pConfig file exists
-  std::ifstream file(config_file);
-  if (!file.good()) {
-    char buff[1024];
-    snprintf(buff, sizeof(buff),
-              "%s file is missing.", config_file.c_str());
-    rvs::logger::Err(buff, MODULE_NAME_CAPS);
-    return -1;
-  } else {
-    file.close();
+  if (!rvs::options::has_option("-r", &val)) {
+
+    // check -c option
+    if (rvs::options::has_option("-c", &val)) {
+
+      config_file = val;
+
+    } else {
+      config_file = "../share/rocm-validation-suite/conf/rvs.conf";
+      // Check if pConfig file exist if not use old path for backward compatibility
+      std::ifstream file(path + config_file);
+      if (!file.good()) {
+        config_file = "conf/rvs.conf";
+      }
+      file.close();
+      config_file = path + config_file;
+    }
+
+    // Check if pConfig file exists
+    std::ifstream file(config_file);
+    if (!file.good()) {
+      char buff[1024];
+      snprintf(buff, sizeof(buff),
+          "%s file is missing.", config_file.c_str());
+      rvs::logger::Err(buff, MODULE_NAME_CAPS);
+      return -1;
+    } else {
+      file.close();
+    }
   }
 
   // check -n options
