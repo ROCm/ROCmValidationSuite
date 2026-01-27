45d36a2cb623736a7645a033733316eff7208c5f
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2026-01-28 08:29:45 +0900

    refactor!: migrate to python3
    fix #1081

--- regression/make_pbqt_conf.py
+++ regression/make_pbqt_conf.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 
-# This script only creates valid combinations, invalid ones will be created as special cases
-from __future__ import print_function
-
 import os
 import itertools
 import sys
